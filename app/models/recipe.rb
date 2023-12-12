07f4a72ebe909a69f887fb396b6aed2ea7eec7aa
Mazin <code.mazin.m@gmail.com>
2023-12-12 13:26:47 +1100

    recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
-    belongs_to :user
-  
-    validates :title, presence: true
-    validates :instructions, length: { minimum: 50 }
-  end
+  belongs_to :user
+
+  validates :title, presence: true
+  validates :instructions, length: { minimum: 50 }
+end
